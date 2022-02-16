796af851563f2d77142047f27aefa368f08b9a82
Aslı Koçyiğit <33804678+kocyigitasli2@users.noreply.github.com>
2022-02-16 19:06:15 +0300

    week1

---
+++ basic/basic/Controllers/cities.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class cities : ControllerBase
+    {
+        
+       private string[] cities= new string[] { "antalya", "istanbul", "izmir" };
+    //domainadresi/api/cities
+
+    public string[] Get()
+        {
+        return cities;
+        }
+
+        public string Get(int id)
+        {
+        return cities[id];
+        }
+    }
+}
